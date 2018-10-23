bc67fff983c2d1d1e9f1585f72bd8ea1f0668835
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-10-22 23:09:08 -0400

    Complete homework with dropdown and hover effect

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-var button = document.getElementById('greenbutton');
-var button = document.getElementById('orangebutton');
-
+var button = document.getElementsByClassName('greenbutton');
 function changeColor() {
-  this.classList.toggle('change-color');
+	this.classList.toggle('change-color');
+};
+for (i=0; i < button.length; i++) {
+  button[i].addEventListener('mouseenter', changeColor);
+  button[i].addEventListener('mouseleave', changeColor);
+  button[i].addEventListener('click', showDiv);
 };
 
-button.addEventListener('mouseenter', changeColor);
-button.addEventListener('mouseleave', changeColor);
+var button = document.getElementsByClassName('orangebutton');
+function changeColor() {
+	this.classList.toggle('change-color');
+};
+for (i=0; i < button.length; i++) {
+ button[i].addEventListener('mouseenter', changeColor);
+ button[i].addEventListener('mouseleave', changeColor);
+};
 
 
 var container = document.getElementsByClassName('container');
@@ -18,4 +27,26 @@ for (i=0; i < container.length; i++) {
   container[i].addEventListener('mouseenter', changeOpacity);
   container[i].addEventListener('mouseleave', changeOpacity);
 };
+
+var locationDiv = document.getElementsByClassName('hide-div');
+function showDiv() {
+  this.nextElementSibling.classList.toggle('show-div');
+  //Get THIS button's next sibling and toggle ITS class
+
+  if (this.nextElementSibling.classList.contains('show-div')) {
+    //If THIS button's next sibling has the class show-div
+
+    this.innerHTML = "CLOSE LOCATIONS";
+    //Make THIS button's innerHTML equal to Close Bio
+
+  } else {
+    this.innerHTML = "FIND A LOCATION";
+    //Otherwise, make THIS button's innerHTML equal to Read Bio
+  }
+};
+
+
+
+
+
 
